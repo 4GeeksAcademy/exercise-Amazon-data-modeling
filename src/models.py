a0c31cb4371b989c756f099c3be6d1839956e836
Rodrigou0 <127239242+Rodrigou0@users.noreply.github.com>
2023-07-10 22:56:50 +0000

    amazon

--- src/models.py
+++ src/models.py
@@ -1,29 +1,54 @@
 import os
 import sys
-from sqlalchemy import Column, ForeignKey, Integer, String
+from sqlalchemy import Column, ForeignKey, Integer,Float, String, DateTime, Enum,ForeignKey
 from sqlalchemy.orm import relationship, declarative_base
 from sqlalchemy import create_engine
 from eralchemy2 import render_er
 
 Base = declarative_base()
 
-class Person(Base):
-    __tablename__ = 'person'
-    # Here we define columns for the table person
-    # Notice that each column is also a normal Python instance attribute.
-    id = Column(Integer, primary_key=True)
-    name = Column(String(250), nullable=False)
+class myEnum(enum.Enum):
+    refund = "refund"
+    paid = "paid"
+    cancelled = "cancelled"
 
-class Address(Base):
-    __tablename__ = 'address'
-    # Here we define columns for the table address.
-    # Notice that each column is also a normal Python instance attribute.
-    id = Column(Integer, primary_key=True)
-    street_name = Column(String(250))
-    street_number = Column(String(250))
-    post_code = Column(String(250), nullable=False)
-    person_id = Column(Integer, ForeignKey('person.id'))
-    person = relationship(Person)
+class Product(Base):
+    __tablename__='product'
+    id = Column(Integer,primary_key=True)
+    name = Column(String(250),nullable=False)
+    pricing = Column(Float, nullable=False)
+    weight = Column(Float, nullable=False)
+    color = Column(String(250),nullable=False)
+    shopping_cart = Column(Integer, ForeignKey("shopping_cart.id"))
+    status = Column(Enum(myEnum))
+
+
+
+    class Customer(Base):
+        __tablename__='customer'
+        id = Column(Integer,primary_key=True)
+        first_name = Column(String(250), nullable=False)
+        last_name = Column(String(250), nullable=False)
+        email= Column(String(250), nullable=False, unique=True)
+        address= Column(String(250), nullable=False)
+        shopping_cart_id= Column(Integer,ForeignKey("shopping_cart.id"))
+
+    class Shopping_Cart(Base):
+        __tablename__='shopping_cart'
+        id = Column(Integer,primary_key=True)
+        quantity= Column(Integer)
+        price= Column(Float)
+        # bill_id= Column(Integer,ForeignKey("bill.id"), primary_key=True)
+
+
+    class Bill(Base):
+        __tablename__='bill'
+        id = Column(Integer,primary_key=True)
+        created_at = Column(DateTime)
+        total_price = Column(Float)
+        status = Column(String,Enum("paid", "pending", "refunded", "cancelled"))
+        shopping_cart_id= Column(Integer,ForeignKey("shopping_cart.id"))
+
 
     def to_dict(self):
         return {}
